3ea966bf998282363a7827c1d3bc17fba9da0da0
ujjwal-2504 <code.with.ujjwal.25@gmail.com>
2025-06-05 20:15:54 +0530

    Appwite and Environment Variable setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>Blog app with app write</h1>
+    </>
+  );
+}
+
+export default App;
