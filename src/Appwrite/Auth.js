bf805b1e78cddc635ea5b98ff9d3bca273b904c0
ujjwal-2504 <code.with.ujjwal.25@gmail.com>
2025-06-06 09:12:58 +0530

    Build Auth service functions

---
+++ src/Appwrite/Auth.js
@@ -0,0 +1,89 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+
+    this.account = new Account(account);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        // call another method
+        return this.login(email, password);
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.log("Appwrite service :: createAccount :: error ", error);
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      const response = await this.account.createEmailPasswordSession(
+        email,
+        password
+      );
+
+      return response;
+    } catch (error) {
+      console.log("Appwrite service :: login :: error ", error);
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      const acc = await this.account.get();
+      if (acc) return acc;
+      else throw new Error("Error :: getCurrentUser:", acc);
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: error ", error);
+      throw error;
+    }
+  }
+
+  async logout() {
+    try {
+      const response = await this.account.deleteSession("current");
+
+      if (response) return response;
+      else throw new Error("Error :: logout:", response);
+    } catch (error) {
+      console.log("Appwrite service :: logout :: error ", error);
+      throw error;
+    }
+  }
+
+  async logoutFromAllDevices() {
+    try {
+      const response = await this.account.deleteSessions();
+
+      if (response) return response;
+      else throw new Error("Error :: logoutFromAllDevices:", response);
+    } catch (error) {
+      console.log("Appwrite service :: logoutFromAllDevices :: error ", error);
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
